848ad20876b2b9e214da0052e2332e674fe1ad90
haliltuzcu <007canberk@gmail.com>
2023-05-29 20:47:49 +0300

    models

---
+++ personnel/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Department(models.Model):
+    name = models.CharField(max_length=32)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+                            
