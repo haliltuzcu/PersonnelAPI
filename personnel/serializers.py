fa47613f83382d614e1f281573b882fef0c67ea7
haliltuzcu <007canberk@gmail.com>
2023-05-29 21:41:02 +0300

    serializers

---
+++ personnel/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Department, Personnel
+
+class DepartmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class PersonnelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Personnel
+        fields = '__all__'
