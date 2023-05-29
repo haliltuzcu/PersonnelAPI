74f0ab0739f782036e74a9d3aa38be76b925186e
haliltuzcu <007canberk@gmail.com>
2023-05-29 21:02:07 +0300

    mdls

--- personnel/models.py
+++ personnel/models.py
@@ -6,4 +6,24 @@ class Department(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Personnel(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    GENDER = (
+        ("M","Male"),
+        ("F", "Female"),
+        ("N","Prefer Not To Say"),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER)
+    title
+    salary
+    started
+    department_id
+    user_id
+    created
+    updated
                             
