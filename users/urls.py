3981142f54332d99b31d4dbd59af4b79a09923c1
haliltuzcu <007canberk@gmail.com>
2023-05-30 21:40:53 +0300

    register

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import RagesterView
 
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
+    path('register/', RagesterView.as_view()),
 ]
